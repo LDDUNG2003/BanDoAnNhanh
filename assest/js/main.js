3839b14a2c77b484c4f962c43bcf1e1167fee61b
Lê Đình Dũng <dungldcntt58@gmail.com>
2022-04-04 20:49:13 +0700

    new code funny code elemen

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,10 +1,10 @@
 var nextImg = document.getElementById('slider__next');
 var returnImg = document.getElementById('slider__return')
-
+var hoverMenus = document.getElementsByClassName('slider__list-item');
 var i = 0;
 nextImg.addEventListener('click', function() {
     var imgs = ['./assest/img/4-IMG_104221-680x460-594x427.jpg', './assest/img/4-IMG_104221-680x460-300x160.jpg', './assest/img/photodune-903822-meal-s-300x160.jpg'];
-    i++
+    i++;
 
     function changeImg() {
         if (i < imgs.length) {
